b7bd8640b31fe35059230b7bb5e0439817109107
sand5907 <38893566+sand5907@users.noreply.github.com>
2018-05-01 16:18:14 -0400

    These are the asset and project files

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+
+    //What we are following
+    public Transform target;
+
+    //Zeros out the velocity
+    Vector3 velocity = Vector3.zero;
+
+    //time to follow target
+    public float smoothTime = .15f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPos = target.position;
+
+        //align camera and targets z position
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
+
