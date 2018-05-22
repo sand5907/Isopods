0fdf9472f6a7fdeff41d03909086e78ed9912e7e
allenj.olsen <allenj.olsen@gmail.com>
2018-05-22 15:10:15 -0400

    Rocket, enemy and health.
    Added an enemy behavior, health system for enemies and some rocket logic. TODO: Implement explosion, fine tune speeds, work on the player script.
    NOTE: If you guys plan on having another enemy that needs to take damage, make sure that it implements the IDamageable interface.

---
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Isopods.Interfaces;
+using UnityEngine;
+using Isopods.Constants;
+
+public class EnemyBehavior : MonoBehaviour, IDamageable
+{
+    public float rotationSpeed = 5.0f;
+    private Transform target;
+    public float health { get; set; }
+
+    
+    // Use this for initialization
+    void Start ()
+    {
+        health = ENEMY_CONST.DEFAULT_ENEMY_HEALTH;
+        target = GameObject.FindGameObjectWithTag(PLAYER_CONST.PLAYER_TAG).transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+
+    public void TakeDamage(float damageTaken)
+    {
+        damageTaken = (damageTaken <= 0.0f) ? 0.0f : damageTaken;
+        health -= damageTaken;
+
+        if (health <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/Bazooka.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Bazooka : MonoBehaviour
+{
+    [Range(1.0f, 20.0f)]
+    public float bazookaRotationSpeed = 5.0f;
+
+    [Range(1.0f, 1000.0f)]
+    public float projectileSpeed = 1000.0f;
+
+    public GameObject projectile;
+    public int ammo = 5;
+    
+
+    private Vector2 direction;
+    private Quaternion rotation;
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, bazookaRotationSpeed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+    
+    public void Shoot()
+    {
+        var rocket = Instantiate(projectile, gameObject.GetComponentInChildren<Transform>().position, rotation);
+        rocket.transform.Rotate(0, 0, 90);
+        rocket.AddComponent<Rigidbody2D>().AddForce(direction * projectileSpeed);
+    }
+}
