b69df9df23580ac3fbd63aebcb2479d8e197c0d7
sand5907 <sand5907@kettering.edu>
2018-05-30 00:51:37 -0400

    fixed issues with line endings

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -70,76 +69,8 @@ public class Move : MonoBehaviour
 
         Vector3 theScale = transform.localScale;
 
-        //theScale.x *= -1;
+        theScale.x *= -1;
 
         transform.localScale = theScale;
     }
 }
-=======
-﻿using UnityEngine;
-
-public class Move : MonoBehaviour
-{
-    //Movement speed
-    public float topSpeed = 10f; 
-
-    //direction
-    public bool facingRight = true; 
-
-    //Not grounded
-    bool grounded = false;
-
-    public bool jumped = false;
-
-    //Checks if player is on ground
-    public Transform groundCheck; 
-
-    //how big circle is to check if grounded
-    float groundRadius = 0.4f;
-
-    //Force of jump
-    public float jumpForce = 700f;
-
-    //What Layer is ground
-    public LayerMask whatIsGround;
-
-
-    private void FixedUpdate() //Physics is fixed
-    {
-        //is the player grounded?
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-
-        //get move direction
-        float move = Input.GetAxis("Horizontal");
-
-        //Add velocity to move controls
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
-        if (move > 0 && !facingRight)
-            Flip();
-        else if (move < 0 && facingRight)
-            Flip();
-    }
-
-    private void Update()
-    {
-        jumped = false;
-        if (grounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            jumped = true;
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-        }
-    }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector3 theScale = transform.localScale;
-
-        //theScale.x *= -1;
-
-        transform.localScale = theScale;
-    }
-}
->>>>>>> 7f99d6e20c68c025ba56e0152a40eae436530b4f
