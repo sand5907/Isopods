63300b004591a98e6477715fe3bd8468f7b1f56b
SCOTT-LAPTOP\Scott <smkramer315.gmail.com>
2018-06-06 00:24:22 -0400

    Added scoring to end of game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,17 @@
-﻿public static class GameController
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
 {
+	public Text scoreText;
     public static int score = 0;
+
+	void Start() 
+	{
+		UpdateScore ();
+	}
+
+	void UpdateScore() {
+		scoreText.text = "You win!\nScore: " + score;
+	}
 }
